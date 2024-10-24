c6fe4887cfc4c7e8315aa367a6369ee0ad27f115
musateduardbogdan <smetytech@gmail.com>
2024-10-24 23:13:31 +0300

    Courses layout | WIP

--- src/lib/controllers/courses.controller.ts
+++ src/lib/controllers/courses.controller.ts
@@ -1,23 +1,7 @@
-import type { ICourse, IExtendedCourse } from '$lib/interfaces/course.interface';
+import type { ICourse } from '$lib/interfaces/course.interface';
 
 export async function getCourses(subdomainId: string): Promise<Array<ICourse>> {
-	const url = '/api/courses/';
-	const method = 'GET';
-	const headers = { 'Content-Type': 'application/json' };
-	const body = JSON.stringify({ subdomainId });
-
-	const response = await fetch(url, { method, headers, body });
-	const data = await response.json();
-
-	if (!response.ok) {
-		throw data;
-	}
-
-	return data;
-}
-
-export async function getCourse(id: string): Promise<IExtendedCourse> {
-	const url = `/api/courses/${id}`;
+	const url = `/api/courses?subdomainId=${subdomainId}`;
 	const method = 'GET';
 	const headers = { 'Content-Type': 'application/json' };
 
---
+++ src/routes/domains/[domainId]/subdomains/[subdomainId]/courses/[courseId]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { marked } from 'marked';
+
+	export let data;
+
+	$: ({ title, content } = data.course);
+</script>
+
+<svelte:head>
+	<title>{title} | Synzo.</title>
+</svelte:head>
+
+<div class="prose dark:prose-invert max-w-full p-6">
+	{@html marked(content)}
+</div>
